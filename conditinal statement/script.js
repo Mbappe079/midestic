c708306a26c150ca38a493f8f271ad214ddc6c76
midestic <badmususman50@gmail.com>
2025-05-05 21:42:22 +0100

    b

---
+++ conditinal statement/script.js
@@ -0,0 +1,12 @@
+// if
+// if else
+// if else if
+
+// let storedPassword = 12345;
+// let inputPassword = 12;
+
+// if (storedPassword == inputPassword) {
+//   alert("password is correct");
+// } else {
+//   alert("password incorrect");
+// }
