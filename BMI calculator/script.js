e2cfefe0fa7bf3c4c76854053d75992b66ff0a8c
midestic <badmususman50@gmail.com>
2025-05-08 11:21:30 +0100

    fixed

--- BMI calculator/script.js
+++ BMI calculator/script.js
@@ -4,11 +4,11 @@ let height = parseInt(prompt(`Input your height`));
 let result = weight / (height * height);
 
 if (result < 18.5) {
-  alert(`${result} means you are underweight `);
+  alert(`${result.toFixed(2)} means you are underweight `);
 } else if (result == 18.5 || result <= 24.9) {
-  alert(`${result} shows that your weight is normal`);
+  alert(`${result.toFixed(2)} shows that your weight is normal`);
 } else if (result == 25 || result <= 29.9) {
-  alert(`${result} shows that you are overweight werey .. go gym🤣`);
+  alert(`${result.toFixed(2)} shows that you are overweight werey .. go gym🤣`);
 } else if (result >= 30) {
-  alert(`${result} omoh obesity go kill you o 😂😂`);
+  alert(`${result.toFixed(2)} omoh obesity go kill you o 😂😂`);
 }
