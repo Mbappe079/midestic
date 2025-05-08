480d22ece5bea30358fd8e5dd61d71b0622a9881
Mbappe079 <adeniyitomiwa4@gmail.com>
2025-05-08 15:01:01 +0100

    1

--- intro to JS/script.js
+++ intro to JS/script.js
@@ -1,4 +1,20 @@
-let heading = document.getElementById("title");
-heading.style.color = "red";
-heading.style.fontSize = "200px";
-console.log(heading);
+// let heading = document.getElementById("title");
+// heading.style.color = "red";
+// heading.style.fontSize = "200px";
+// console.log(heading);
+
+// let storedPaasword = ;
+// let typedPassword = ;
+
+// if (storedPaasword == typedPassword) {
+//   alert("password is correct");
+// } else {
+//   alert("password is incorrect");
+// }
+let Y = parseInt(prompt("What's the average score?"));
+let Z = parseInt(prompt("What's your score?"));
+if (Y < Z) {
+  alert(`${studentScore} is less than ${average} so you fail`);
+} else {
+  alert("You pass");
+}
