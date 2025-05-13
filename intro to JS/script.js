ef51d1de80b79f4e5eda8732fd450728e80cf665
Mbappe079 <adeniyitomiwa4@gmail.com>
2025-05-13 15:47:05 +0100

    2

--- intro to JS/script.js
+++ intro to JS/script.js
@@ -11,10 +11,27 @@
 // } else {
 //   alert("password is incorrect");
 // }
-let Y = parseInt(prompt("What's the average score?"));
-let Z = parseInt(prompt("What's your score?"));
-if (Y < Z) {
-  alert(`${studentScore} is less than ${average} so you fail`);
-} else {
-  alert("You pass");
-}
+// let Y = parseInt(prompt("What's the average score?"));
+// let Z = parseInt(prompt("What's your score?"));
+// if (Y < Z) {
+//   alert(`${studentScore} is less than ${average} so you fail`);
+// } else {
+//   alert("You pass");
+// }
+
+// let myStr = "    Hello, Javascript World!    ";
+
+// console.log(myStr);
+// console.log(myStr.toUpperCase());
+// console.log(myStr.toLowerCase());
+// console.log(myStr.charAt());
+// console.log(myStr.indexOf("Javascript"));
+// console.log(myStr.includes("Hello"));
+// console.log(myStr.slice(2, 8));
+// console.log(myStr.trim());
+// console.log(myStr.replace("World", "Universe"));
+// console.log(myStr.split("  "));
+// console.log(myStr.split("  "))[1];
+// comsole.log(myStr.startsWith("Hello"));
+// console.log(myStr.endsWith("Hi"));
+// console.log(myStr);
